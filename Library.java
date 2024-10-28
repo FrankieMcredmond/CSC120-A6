cf5f7a7f28ff9482fd0de0e50031a18dfcae3ba6
Frankie <emcredmond@smith.edu>
2024-10-28 13:55:52 -0400

    Library pt. 1

--- Library.java
+++ Library.java
@@ -1,12 +1,15 @@
-/* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
+public class Library extends Building{
+    private Hashtable<String, boolean> collection;
 
-    public Library() {
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
       System.out.println("You have built a library: 📖");
+      this.catalog= new Hashtable<String, boolean>();
     }
   
     public static void main(String[] args) {
-      new Library();
+      new Library("Main Library", "7 Neilson Drive, Northampton, MA 01063", 4);
     }
   
   }
