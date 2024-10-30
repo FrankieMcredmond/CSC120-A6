0d6abdb81cae9826b47339103b2656f598f0fbea
Frankie <emcredmond@smith.edu>
2024-10-29 22:23:50 -0400

    library pt. 3

--- Cafe.java
+++ Cafe.java
@@ -1,7 +1,11 @@
 /* This is a stub for the Cafe class */
-public class Cafe {
-
-    public Cafe() {
+public class Cafe extends Building{
+    private int nCoffeeOunces; // The number of ounces of coffee remaining in inventory
+    private int nSugarPackets; // The number of sugar packets remaining in inventory
+    private int nCreams; // The number of "splashes" of cream remaining in inventory
+    private int nCups; // The number of cups remaining in inventory
+    public Cafe(String name, String address, int nFloors) {
+        super(name, address, nFloors);
         System.out.println("You have built a cafe: ☕");
     }
     
--- Library.java
+++ Library.java
@@ -1,15 +1,73 @@
 import java.util.Hashtable;
 public class Library extends Building{
-    private Hashtable<String, boolean> collection;
+    private Hashtable<String, Boolean> collection;
 
     public Library(String name, String address, int nFloors) {
       super(name, address, nFloors);
       System.out.println("You have built a library: 📖");
-      this.catalog= new Hashtable<String, boolean>();
+      this.collection= new Hashtable<>();
     }
-  
+
+    public void addTitle(String title){
+      if (collection.containsKey(title)==false){
+        collection.put(title, true);}
+      else {
+        System.out.println("title already found in collection");}}
+
+    public String removeTitle(String title){
+      if (collection.containsKey(title)==true){
+        collection.remove(title);
+        return (title+" has been removed from the collection");}
+      else {
+        return("title not found in collection");}
+    }
+
+    public void checkOut(String title){
+      if (collection.containsKey(title)==true){
+        if (collection.get(title)==true){
+          collection.replace(title, true, false);}}
+    }
+
+    public void returnBook(String title){
+      if (collection.containsKey(title)==true){
+        if (collection.get(title)==false){
+          collection.replace(title, false, true);}}
+    }
+
+    public boolean containsTitle(String title){
+      return (collection.containsKey(title));
+    }
+
+    public boolean isAvailable(String title){
+      if (collection.containsKey(title)==true){
+        return collection.get(title);}
+      else{ return false;}
+    }
+
+    public void printCollection(){
+      System.out.println("\n");
+      System.out.println("Welcome to "+this.name+"! Here are the books in our Collection!\n");
+      collection.forEach( (k, v) -> { 
+        System.out.print(k);
+        if (collection.get(k)== true){
+          System.out.print(": available");}
+        else {
+          System.out.print(": not available");}
+        System.out.println("\n");
+        });
+    }
     public static void main(String[] args) {
-      new Library("Main Library", "7 Neilson Drive, Northampton, MA 01063", 4);
+      Library Neilson= new Library("Neilson", "7 Neilson Drive, Northampton, MA 01063", 4);
+      System.out.println(Neilson);
+      Neilson.addTitle("Catcher and the rye");
+      Neilson.addTitle("Junie B.Jones");
+      Neilson.addTitle("Cat in the Hat");
+      Neilson.addTitle("Wrinkle in Time");
+      Neilson.removeTitle("Catcher and the rye");
+      Neilson.checkOut("Cat in the Hat");
+      Neilson.printCollection();
+      System.out.println(Neilson.containsTitle("Catcher and the rye"));
+      System.out.println(Neilson.isAvailable("Cat in the Hat"));
     }
   
   }
